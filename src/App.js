b73512654482e9740e227a03223f3e8849b3631b
Leticia <leticia.bora@gmail.com>
2021-03-09 11:48:27 -0300

    estrutura

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { animals } from './data';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super(); 
+    this.state = {
+      searchName: '',
+      allAnimals: animals,
+      mood: 'angry',
+    }
+  }
+
+  filterName() {
+    const { searchName } = this.state;
+    let filterAnimals = animals;
+
+    filterAnimals = animals.filter((animal) => animal.name.includes((searchName)))
+    this.setState({
+      allAnimals: filterAnimals
+    })
+    return filterAnimals;
+  }
+
+  filterMood() {
+    console.log('filtrou')
+  }
+
+  render() {
+    const { searchName, allAnimals } = this.state;
+    return (
+      <div className="App">
+       <label>
+        Digite nome
+        <input
+          type="text"
+          value={ searchName }
+          onChange={ (e) => this.setState({ searchName: e.target.value }, () => this.filterName())}
+        />
+       </label>
+       <label>
+         <select onChange={ (e) => this.setState({ mood: e.target.value }) }>
+           <option value="angry">Angry</option>
+           <option value="cute">Cute</option>
+           <option value="happy">Happy</option>
+           <option value="lazy">Lazy</option>
+         </select>
+       </label>
+       <button type="button" onClick={ () => this.filterMood() }>Filtrar</button>
+  
+       <table>
+          <thead>
+            <tr>
+              <th>Foto</th>
+              <th>Tipo</th>
+              <th>Nome</th>
+              <th>Humor</th>
+            </tr>
+          </thead>
+          <tbody>
+            { allAnimals.map((animal, index) => (
+              <tr key={ index }>
+                <td><img className="image" src={ animal.url } alt={ animal.name } /></td>
+                <td>{ animal.type }</td>
+                <td>{ animal.name }</td>
+                <td>{ animal.mood }</td>
+              </tr>
+            )) }
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,134 @@
+export const animals = [
+  {
+    type: 'cat',
+    name: 'Jade',
+    url: './images/Jade.jpeg',
+    mood: 'angry'
+  },
+  {
+    type: 'cat',
+    name: 'Jade',
+    url: './images/Jade2.jpeg',
+    mood: 'angry'
+  },
+  {
+    type: 'cat',
+    name: 'Jade',
+    url: './images/Jade3.jpeg',
+    mood: 'angry'
+  },
+  {
+    type: 'cat',
+    name: 'Jerry',
+    url: './images/Jerry.jpeg',
+    mood: 'cute',
+  },
+  {
+    type: 'cat',
+    name: 'Griffin',
+    url: './images/Griffin.jpeg',
+    mood: 'lazy',
+  },
+  {
+    type: 'cat',
+    name: 'Griffin',
+    url: './images/Griffin2.jpeg',
+    mood: 'lazy',
+  },
+  {
+    type: 'dog',
+    name: 'Bóris',
+    url: './images/Boris1.jpg',
+    mood: 'hungry',
+  },
+  {
+    type: 'dog',
+    name: 'Bóris',
+    url: './images/Boris2.jpg',
+    mood: 'hungry',
+  },
+  {
+    type: 'dog',
+    name: 'Bóris',
+    url: './images/Boris3.jpg',
+    mood: 'cute',
+  }, 
+  {
+    type: 'dog',
+    name: 'Brigitte Bardot',
+    url: './images/Brigitte.jpg',
+    mood: 'cute',
+  },
+  {
+    type: 'dog',
+    name: 'Brigitte Bardot',
+    url: './images/Brigitte2.jpg',
+    mood: 'happy',
+  },
+  {
+    type: 'dog',
+    name: 'Laika',
+    url: './images/Laika1.jpeg',
+    mood: 'hungry',
+  },
+  {
+    type: 'dog',
+    name: 'Laika',
+    url: './images/Laika2.jpeg',
+    mood: 'lazy',
+  },
+  {
+    type: 'dog',
+    name: 'Leao',
+    url: './images/Leao.jpeg',
+    mood: 'angry',
+  },
+  {
+    type: 'dog',
+    name: 'Luna',
+    url: './images/Luna.jpeg',
+    mood: 'lazy',
+  },
+  {
+    type: 'dog',
+    name: 'Estopinha',
+    url: './images/Estopinha.jpg',
+    mood: 'lazy',
+  },
+  {
+    type: 'dog',
+    name: 'Madonna',
+    url: './images/Madonna.jpg',
+    mood: 'lazy',
+  },
+  {
+    type: 'dog',
+    name: 'Teemo',
+    url: './images/Teemo.jpeg',
+    mood: 'hungry',
+  },
+  {
+    type: 'dog',
+    name: 'Teemo',
+    url: './images/Teemo2.jpeg',
+    mood: 'lazy',
+  },
+  {
+    type: 'dog',
+    name: 'Margareth',
+    url: './images/Margareth.jpeg',
+    mood: 'happy',
+  },
+  {
+    type: 'chicken',
+    name: 'Benedita',
+    url: './images/Benedita.jpeg',
+    mood: 'happy',
+  },
+  {
+    type: 'chicken',
+    name: 'Benedita',
+    url: './images/Benedita2.jpeg',
+    mood: 'hungry',
+  }
+];
